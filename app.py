5c1a2cfa649ecda61de369254acd386e2fc970ed
Nelson <nelsontorresjr330>
2019-12-07 16:20:03 -0600

    test

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # import necessary libraries
 from flask import (
     Flask,
@@ -10,11 +9,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///SQL/query.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Resourcesz\wild_life.db"
 
 db = SQLAlchemy(app)
 
-class Wild_Life(Base):
+print(db)
+
+class Wild_Life(db.Model):
     __tablename__ = "Wild_Life"
     id = db.Column(db.Integer, primary_key=True)
     iucn = db.Column(db.String(255))
@@ -22,12 +23,43 @@ class Wild_Life(Base):
     cou = db.Column(db.String(255))
     value = db.Column(db.String(255))
 
+    def __repr__(self):
+        return '<%r>' % (self.cou)
 
-=======
+@app.before_first_request
+def setup():
+    # Recreate database each time for demo
+    db.drop_all()
+    db.create_all()
+
+@app.route("/api/data")
+def list_pets():
+    results = db.session.query(Wild_Life.iucn, Wild_Life.spec, Wild_Life.cou, Wild_Life.value).all()
+
+    test = []
+    print(results)
+
+    for result in results:
+        test.append({
+            "iucn": result[0],
+            "spec": result[1],
+            "coun": result[2],
+            "valu": result[3]
+        })
+    return jsonify(test)
+
+@app.route("/")
+def home():
+    return "Welcome!"
+
+
+if __name__ == "__main__":
+    app.run()
+'''
 import sqlite3
 from flask import g
 
-DATABASE = '/path/to/database.db'
+DATABASE = '/SQL/wild_life.db'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -40,4 +72,4 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
->>>>>>> f0afe1a59c2d6e7751a492e6d36546779ea15d6a
+'''
