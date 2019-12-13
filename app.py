e4d3c4a390f2e25447a92feacd9da7226f9c78a8
daniel mowbray <danielmowbray@mac.com>
2019-12-12 22:56:26 -0600

    so much winning

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import os
 # import necessary libraries
 from flask import (
     Flask,
     render_template,
     jsonify,
-    request)
-
+    request,
+    send_from_directory)
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 #from sqlalchemy.ext.automap import automap_base
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = 'static')
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Resources/wild_life.db"
 
@@ -16,19 +19,31 @@ db = SQLAlchemy(app)
 
 status = db.Table('status', db.metadata, autoload=True, autoload_with=db.engine)
 
-@app.route('/assests/critical')
+@app.route('/')
 def index():
+    return app.send_static_file("index.html")
+
+@app.route('/images/<path:filename>')
+def serve_static_images(filename):
+    return send_from_directory(os.path.join(".", 'static/images'), filename)
+
+@app.route('/js/<path:filename>')
+def serve_static_js(filename):
+    return send_from_directory(os.path.join(".", 'static/js'), filename)
+
+@app.route('/vulnerable')
+def vulnerable():
     results = db.session.query(status).all()
     for r in results:
         print(r.SPEC)
 
-    return ''
+    return jsonify(results)
 
 @app.route("/api/test")
 def test():
-    test = db.sessions.query(status,IUCN, status.SPEC, status.COU, status.Value).all()
+    test = db.sessions.query(status.IUCN, status.SPEC, status.COU, status.Value).all()
     return jsonify(test)
 
 
 
-app.run()
+app.run(host="0.0.0.0")
